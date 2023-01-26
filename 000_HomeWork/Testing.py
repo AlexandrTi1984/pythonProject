591472802087a36597dbb71874ff586bc8a45a4c
AlexandrTi1984 <warbeavis84@gmail.com>
2023-01-26 20:59:16 +0200

    test

---
+++ 000_HomeWork/Testing.py
@@ -0,0 +1,5 @@
+#1
+user_name = input('Введите имя')
+user_Familia =input('Введите фамилию')
+user_age = input('Введите возраст')
+print('Hello, ' + user_name + ' ' + user_Familia + '.Your age is ' + user_aAlexge)
